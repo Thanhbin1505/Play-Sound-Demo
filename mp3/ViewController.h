ccb1245431f0a25fe812a66b10817fbf7565c4ba
linhnguyen <linhnguyen1805@gmail>
2013-09-25 21:03:19 +0700

    Initial Commit

---
+++ mp3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mp3
+//
+//  Created by linhnguyen on 9/25/13.
+//  Copyright (c) 2013 viettel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ mp3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  mp3
+//
+//  Created by linhnguyen on 9/25/13.
+//  Copyright (c) 2013 viettel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
